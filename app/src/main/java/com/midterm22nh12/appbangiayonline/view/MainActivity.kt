79af8e4e030dfe819cb7e13a8cc623018c27fa64
nguyenduytay <duytayx8@gmail.com>
2025-04-06 10:19:14 +0700

    duy tây đẩy lên 6/4/2025 10:19

--- app/src/main/java/com/midterm22nh12/appbangiayonline/view/MainActivity.kt
+++ app/src/main/java/com/midterm22nh12/appbangiayonline/view/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.midterm22nh12.appbangiayonline
+package com.midterm22nh12.appbangiayonline.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.midterm22nh12.appbangiayonline.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
