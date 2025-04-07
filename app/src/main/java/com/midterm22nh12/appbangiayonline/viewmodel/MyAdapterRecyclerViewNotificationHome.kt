15ef989e4213b6f1b46f720ee836bc32c3032d0a
nguyenduytay <duytayx8@gmail.com>
2025-04-07 21:25:57 +0700

    duy tây đẩy lên 7/4/2025 21:25

--- app/src/main/java/com/midterm22nh12/appbangiayonline/viewmodel/MyAdapterRecyclerViewNotificationHome.kt
+++ app/src/main/java/com/midterm22nh12/appbangiayonline/viewmodel/MyAdapterRecyclerViewNotificationHome.kt
@@ -5,13 +5,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.midterm22nh12.appbangiayonline.databinding.ItemRecyclerViewNotificationHomeBinding
 import com.midterm22nh12.appbangiayonline.model.ItemRecyclerViewNotificationHome
 
 class MyAdapterRecyclerViewNotificationHome(private val itemList : List<ItemRecyclerViewNotificationHome>)
     :RecyclerView.Adapter<MyAdapterRecyclerViewNotificationHome.MyViewHolder>()
 {
-        private lateinit var bindingItemRecyclerViewNotificationHome : ItemRecyclerViewNotificationHomeBinding
+    private lateinit var bindingItemRecyclerViewNotificationHome : ItemRecyclerViewNotificationHomeBinding
     class MyViewHolder(val binding: ItemRecyclerViewNotificationHomeBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
@@ -22,6 +23,8 @@ class MyAdapterRecyclerViewNotificationHome(private val itemList : List<ItemRecy
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
        bindingItemRecyclerViewNotificationHome.tvTitleNotificationHome.text = itemList[position].title
        bindingItemRecyclerViewNotificationHome.tvContentNotificationHome.text = itemList[position].content
+       // Glide.with(bindingItemRecyclerViewNotificationHome.ivNotification.context).load(itemList.imageUrl).into(imageViewItem)
+        bindingItemRecyclerViewNotificationHome.ivNotification.setBackgroundResource(itemList[position].image)
     }
 
     override fun getItemCount() = itemList.size
