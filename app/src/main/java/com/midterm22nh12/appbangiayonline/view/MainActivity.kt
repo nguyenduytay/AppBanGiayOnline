0b3cf73c3ada177c77517f094546291786e49aee
lynguyen111 <lynguyen21324@gmail.com>
2025-04-08 05:49:23 +0700

    Ly day len 5:49  8/4/2025

--- app/src/main/java/com/midterm22nh12/appbangiayonline/view/MainActivity.kt
+++ app/src/main/java/com/midterm22nh12/appbangiayonline/view/MainActivity.kt
@@ -15,8 +15,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         bindingMainActivity = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingMainActivity.root)
-//          setContentView(R.layout.create_account)
+
         setUpViewpager2()
+
+
+//         setContentView(R.layout.create_account)
+//        setContentView(R.layout.login)
+
     }
     //sự kiện chuyển đổi fragment
     private fun setUpViewpager2() {
