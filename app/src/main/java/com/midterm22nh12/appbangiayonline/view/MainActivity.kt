a402e44f72d54f65e5eea37217fd2187924cadb9
lynguyen111 <lynguyen21324@gmail.com>
2025-04-07 13:20:18 +0700

    Ly day len 1:20 7/4/2025

--- app/src/main/java/com/midterm22nh12/appbangiayonline/view/MainActivity.kt
+++ app/src/main/java/com/midterm22nh12/appbangiayonline/view/MainActivity.kt
@@ -3,19 +3,13 @@ package com.midterm22nh12.appbangiayonline.view
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.midterm22nh12.appbangiayonline.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(R.layout.create_account)
+
         }
     }
-}
