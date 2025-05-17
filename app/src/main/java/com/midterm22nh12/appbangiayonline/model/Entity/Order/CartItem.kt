a3cd731c84c5b40d241d2f6c98ff23b060c8dc3f
nguyenduytay <duytayx8@gmail.com>
2025-05-17 15:07:10 +0700

    Tây - Docs : Cập nhật mới 17/5/2025 15:07

--- app/src/main/java/com/midterm22nh12/appbangiayonline/model/Entity/Order/CartItem.kt
+++ app/src/main/java/com/midterm22nh12/appbangiayonline/model/Entity/Order/CartItem.kt
@@ -1,14 +1,14 @@
-    package com.midterm22nh12.appbangiayonline.model.Entity.Order
+package com.midterm22nh12.appbangiayonline.model.Entity.Order
 
-    import com.midterm22nh12.appbangiayonline.model.Entity.Product.Product
-    import com.midterm22nh12.appbangiayonline.model.Entity.Product.ProductColor
+import com.midterm22nh12.appbangiayonline.model.Entity.Product.Product
+import com.midterm22nh12.appbangiayonline.model.Entity.Product.ProductColor
 
-    /**
-     * Lớp đại diện cho một sản phẩm trong giỏ hàng
-     */
-    data class CartItem(
-        val product: Product,
-        val selectedColor: ProductColor,
-        val selectedSize: String,
-        val quantity: Int = 1
-    )
+/**
+ * Lớp đại diện cho một sản phẩm trong giỏ hàng
+ */
+data class CartItem(
+    val product: Product,           // Thông tin sản phẩm (ID, tên, giá, mô tả, v.v.)
+    val selectedColor: ProductColor, // Màu sắc được chọn cho sản phẩm
+    val selectedSize: String,       // Kích thước được chọn cho sản phẩm
+    val quantity: Int = 1           // Số lượng sản phẩm, mặc định là 1
+)
